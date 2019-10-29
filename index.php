ba0706905d333675c84585b93094e5ebe95a6c4c
DeividasBirzys <birzys.deividas@gmail.com>
2019-10-29 12:33:58 +0200

    index

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 
 <html>
   <head>
-      <title> PHP lydes ir <?php print date('l')+1 . '!';?> </title>
+      <title> PHP lydes ir <?php print date('l', strtotime ('+1 day')) . '!';?> </title>
   </head>
   <body>
-      <h1><strong>Deividas</strong> - PHP su manim buvo ir <?php print date ('H')-1 . ' valanda' ; ?> </h1>
-    <p> <?php print date('Y')+1; ?> ne uz kalnu </p>
+      <h1><strong>Deividas</strong> - PHP su manim buvo ir <?php print date ('H', strtotime('-1 hour')) . ' valanda' ; ?> </h1>
+    <p> <?php print date('Y', strtotime ('+1 year')); ?> ne uz kalnu </p>
     
   </body>
 </html>
