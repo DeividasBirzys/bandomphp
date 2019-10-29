e961b030e43003e1b6a9cac3501fa5bafae48413
DeividasBirzys <birzys.deividas@gmail.com>
2019-10-29 10:31:07 +0200

    index

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-print 'helloooo'; 
+print 'helloooo';
+?>
+<html>
+  <head>
+
+    <title>As valdau HTML!</title>
+  </head>
+  <body>
+    <h1>Deividas - HTML asas!</h1>
+    <p>Moku kurti statini turini.</p>
+  </body>
+</html>
