176dac6b88fd66d312cc1f041c93fea9da014240
DeividasBirzys <birzys.deividas@gmail.com>
2019-10-29 12:49:55 +0200

    index

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
     <head>
         <title> Komentarai</title>
     </head>
-     <body>
-         <h1> Dainu tekstai; </h1>
-         <h2> Paragrafo daina; </h2>
-         <p> As toks vienisas jaunas paragrafas... </p>
-       <!-- <p> Mindau svetimo kodo gatves... </p>  komentuoju as tau sita paragrafa -->
-         <p> Mindau svetimo kodo gatves... </p> 
+    <body>
+        <h1> Dainu tekstai; </h1>
+        <h2> Paragrafo daina; </h2>
+        <?php
+        //<p> As toks vienisas jaunas paragrafas... </p>//
+        ?>
+      <!-- <p> Mindau svetimo kodo gatves... </p>  komentuoju as tau sita paragrafa -->
     </body>
 </html>
