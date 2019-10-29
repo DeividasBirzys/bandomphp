27bf8dc411db8b4b793d7fc347ee046415e5b74a
DeividasBirzys <birzys.deividas@gmail.com>
2019-10-29 11:05:58 +0200

    html and php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
   </head>
   <body>
     <h1>Deividas - HTML <?php print 'ir PHP asas!'?> </h1>
-    <p>Kuriu dinamini turini <?php print 'php ir PHP!'?> </p>
-    <?php
+    <p>Kuriu dinamini turini html <?php print 'ir PHP!'?> </p>
+   <?php
     print date('2019-10-29 10:53:12')
     ?>
   </body>
