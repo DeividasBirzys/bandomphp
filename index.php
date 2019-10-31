16740cd840918de9c1c143cce3765075d8b37efd
DeividasBirzys <birzys.deividas@gmail.com>
2019-10-31 12:05:31 +0200

    index.php

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
-$Paskola = (rand(1, 100));
-$Grazinimas1 = (rand(101, 200));
-$Grazinimas2 = (rand(201, 300));
-$h2 = "Jei paemei $Paskola jievru";
-$h3 = "Su dviem kabanciais grazinsi $Grazinimas1";
-$h4 = "Su vienu kabanciu grazinsi $Grazinimas2";
+$bin_vol = 40;
+$bin_heap_vol = rand (5, 15);
+$trash_per_day = 15;
+$days = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
 ?>
 <html>
     <head>
         <title>PHP</title>
     </head>
     <body>
-        <h1>Skolos skaiciukle</h1>
-        <h2><?php print $h2; ?></h2>
-        <h3><?php print $h3; ?></h3>
-        <h4><?php print $h4; ?></h4>
+        <h1>Siuksliu prognoze</h1>
+        <p>Turima siuksline - <?php print $bin_vol;?> litru</p>
+        <p>Zmona nieko nesako, kol kaupas nevirsija <?php print $bin_heap_vol;?> litru</p>
+        <h3>Isvada: nieko nedarysiu <?php print $days;?> dienu</h3>
     </body>
 </html>
