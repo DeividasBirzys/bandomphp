713a452a247bf892888252ea3be2b77e71f09760
DeividasBirzys <birzys.deividas@gmail.com>
2019-10-31 12:09:57 +0200

    index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ $bin_vol = 40;
 $bin_heap_vol = rand (5, 15);
 $trash_per_day = 15;
 $days = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
+$Turima_siuksline = "Turima siuksline - $bin_vol litru";
+$Zmona_nesko = "Zmona nieko nesako, kol kaupas nevirsija $bin_heap_vol litru";
+$Isvada = "Isvada: nieko nedarysiu $days dienu";
 ?>
 <html>
     <head>
@@ -10,8 +13,8 @@ $days = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
     </head>
     <body>
         <h1>Siuksliu prognoze</h1>
-        <p>Turima siuksline - <?php print $bin_vol;?> litru</p>
-        <p>Zmona nieko nesako, kol kaupas nevirsija <?php print $bin_heap_vol;?> litru</p>
-        <h3>Isvada: nieko nedarysiu <?php print $days;?> dienu</h3>
+        <p><?php print $Turima_siuksline;?></p>
+        <p><?php print $Zmona_nesko;?></p>
+        <h3><?php print $Isvada;?></h3>
     </body>
 </html>
