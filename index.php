55071b897199bb1a95d9476a1f49656ddeb51a90
DeividasBirzys <birzys.deividas@gmail.com>
2019-10-31 11:34:19 +0200

    index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-
-<?php 
-$img = 'https://www.askideas.com/media/48/Funny-Hillary-Clinton-Surprised-Face-Photo.jpg'
+<?php
+$Paskola = (rand(1, 100));
+$Grazinimas1 = (rand(101, 200));
+$Grazinimas2 = (rand(201, 300));
+$h2 = "Jei paemei $Paskola jievru";
+$h3 = "Su dviem kabanciais grazinsi $Grazinimas1";
+$h4 = "Su vienu kabanciu grazinsi $Grazinimas2";
 ?>
 <html>
     <head>
-        
-        
+        <title>PHP</title>
     </head>
     <body>
-        <img src="<?php print $img;?>"/>
-        <img src="<?php print $img;?>"/>
-        <img src="<?php print $img;?>"/>
-        <img src="<?php print $img;?>"/>
+        <h1>Skolos skaiciukle</h1>
+        <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
+        <h4><?php print $h4; ?></h4>
     </body>
 </html>
