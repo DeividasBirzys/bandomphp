a162483b3fec2c4e1f40d51c08b3c115bd133bb4
DeividasBirzys <birzys.deividas@gmail.com>
2019-11-05 11:56:57 +0200

    sauleta.php

--- index.php
+++ index.php
@@ -1,27 +1,49 @@
 <?php
-$grizai_velai = rand(0, 1);
-$grizai_isgeres = rand(0, 1);
+$sunny = rand(0, 1);
+$rainy = rand(0, 1);
 
-
-if ($grizai_velai && !$grizai_isgeres) {
-    $h_2rezultatas = 'Grizai velai!';
-} else if ($grizai_velai && $grizai_isgeres) {
-    $h_2rezultatas = 'Grizai velai ir isgeres suka';
-} else if (!$grizai_velai && $grizai_isgeres) {
-    $h_2rezultatas = 'Grizai isgeres';
-} else {
-    $h_2rezultatas = 'Nieko nepadarei';
+if ($sunny && !$rainy) {
+    $h1 = 'Sauleta';
+    $class = 'bg-sunny';
+}else if (!$sunny && $rainy){
+    $h1 = 'Lietinga';
+    $class = 'bg-rainy';
+}else if ($sunny && $rainy) {
+    $h1 = 'Debesuota su lietum';
+    $class = 'bg-sunny_rainy';
 }
-
-$h_1 = 'Buitine skaiciuokle';
-$h_2 = "Situacija: $h_2rezultatas";
 ?>
 <html>
     <head>
-        <title>PHP</title>
+        <style>
+            .content {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+            }
+            .bg-img {
+                width: 300px;
+                height: 300px;
+                background-size: cover;
+            }
+            .bg-rainy {
+                background-image: url("https://marshallslocuminn.com/wp-content/uploads/2015/09/creative-rain_01.jpg")
+            }
+            .bg-sunny {
+                background-image: url("http://ipadflava.com/wp-content/uploads/Sun-280x280.jpg");
+            }
+            .bg-sunny_rainy {
+                background-image: url("https://i.pinimg.com/originals/87/5e/8d/875e8d92f22ddc07d12a7506b4ea2780.jpg");
+            }
+        </style>
     </head>
     <body>
-        <h1><?php print ($h_1); ?> </h1>
-        <h2><?php print ($h_2); ?> </h2>
+        <div class="content">
+            <div class="bg-img <?php print $class; ?>"></div>
+            <h1><?php print $h1; ?></h1>
+        </div>
     </body>
 </html>
+
+
+
