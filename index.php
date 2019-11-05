97637bb2c342eb47bd2e94dfe5dc1e65fcd29881
DeividasBirzys <birzys.deividas@gmail.com>
2019-11-05 17:26:25 +0200

    index.php

--- index.php
+++ index.php
@@ -19,13 +19,16 @@ for ($i = 0; $i < $days; $i++) {
 }
 
 $price_ttl = ceil($count_ttl / 20) * $pack_price;
-
+$time_total = floor(($count_ttl * 5) / 60);
+        
 $h1 = 'Mano dumu skaiciuokle';
 $h2 = "Per $days dienas, surukysiu $count_ttl cigareciu uz $price_ttl";
+$h3 = "Viso traukdamas prastovesiu $time_total valandu"
 ?>
 <html>
     <body>
-        <h1><?php print $h1; ?> </h1>
-        <h2><?php print $h2; ?></h2>
+        <h1><?php print $h1;?> </h1>
+        <h2><?php print $h2;?></h2>
+        <h3><?php print $h3;?></h3>
     </body>
 </html>
