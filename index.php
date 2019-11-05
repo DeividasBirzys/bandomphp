837cf56e61a2879bbf081d17606b06614e982e0a
DeividasBirzys <birzys.deividas@gmail.com>
2019-11-05 17:04:11 +0200

    index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 $days = 365;
 $pack_price = 3.50;
 $count_ttl = 0;
-$price_ttl = 0;
 
 for ($i = 0; $i < $days; $i++) {
     $day_of_week = date('N', strtotime('+' . $i . 'day'));
@@ -14,14 +13,15 @@ for ($i = 0; $i < $days; $i++) {
         $cigs_sat = rand(10, 20);
         $count_ttl += $cigs_sat;
     } else {
-        $cigs_mon_fri = rand(3, 4) * 5;
+        $cigs_mon_fri = rand(3, 4);
         $count_ttl += $cigs_mon_fri;
     }
 }
+
 $price_ttl = ceil($count_ttl / 20) * $pack_price;
 
 $h1 = 'Mano dumu skaiciuokle';
-$h2 = "Per $days dienas, surukysiu $count_ttl cigareciu uz $price_ttl"
+$h2 = "Per $days dienas, surukysiu $count_ttl cigareciu uz $price_ttl";
 ?>
 <html>
     <body>
